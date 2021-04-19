4483a61ad86511e358746940efac424fb7a16238
Matiss Janis Aboltins <matiss@mja.lv>
2021-04-19 20:44:17 +0100

    :recycle: (api) use calc_price_per_sqm field rather than price_per_sqm

--- src/lib/brokalys-api.js
+++ src/lib/brokalys-api.js
@@ -5,33 +5,32 @@ export async function getPricesInRange(start, end, filters) {
     process.env.BROKALYS_API_URL,
     {
       query: `
-        {
+        query DataExtraction_PricesInRange($filter: PropertyFilter!) {
           properties(
-            filter: {
-              ${Object.entries(filters).map(
-                ([filter, value]) => `
-                ${filter}: { eq: "${value}" }
-              `,
-              )}
-              ${
-                filters.type === 'rent'
-                  ? 'rent_type: { in: ["monthly", "unknown"] }'
-                  : ''
-              }
-              published_at: {
-                gte: "${start}"
-                lte: "${end}"
-              }
-            },
+            filter: $filter,
             limit: null
           ) {
             results {
               price
-              price_per_sqm
+              calc_price_per_sqm: price_per_sqm
             }
           }
         }
       `,
+      variables: {
+        filter: {
+          ...Object.entries(filters).map(([filter, value]) => ({
+            [filter]: { eq: value },
+          })),
+          ...(filters.type === 'rent'
+            ? { rent_type: { in: ['monthly', 'unknown'] } }
+            : {}),
+          published_at: {
+            gte: start,
+            lte: end,
+          },
+        },
+      },
     },
     {
       headers: {
